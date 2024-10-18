da76d11235f95059f87cbd1d17462a67fa5e1609
webgaurav02 <gauravcodes123@gmail.com>
2024-10-18 16:37:55 +0530

    Fix errors 2

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -1,23 +1,22 @@
-const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
-const path = require('path');
-require('dotenv').config();
+import nodemailer from 'nodemailer';
+import hbs from 'nodemailer-express-handlebars';
+import path from 'path';
 
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
 const handlebarOptions = {
   viewEngine: {
-    partialsDir: path.resolve('./views/'), 
-    defaultLayout: false, 
+    partialsDir: path.resolve('./views/'),
+    defaultLayout: false,
   },
-  viewPath: path.resolve('./views/'), 
-  extName: '.handlebars', 
+  viewPath: path.resolve('./views/'),
+  extName: '.handlebars',
 };
 
 transporter.use('compile', hbs(handlebarOptions));
@@ -27,18 +26,20 @@ async function sendEmail(context) {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_RECEIVE,
     subject: `NEW SUBMISSION FROM MACS WEBSITE: ${context.firstname} ${context.lastname}`,
-    template: 'contact', // Handlebars template
-    context: context, // Data to render in the template
-    // attachments: [
-    //   {
-    //     filename: 'logo_black.png',
-    //     path: 'https://tycindia.com/logo/logo_black.png', // You can also use a local path
-    //     cid: 'logo', // Content ID reference to use in the template
-    //   },
-    // ],
+    template: 'contact',
+    context: context,
   };
 
   return transporter.sendMail(mailOptions);
 }
 
-module.exports = { sendEmail };
+export async function POST(req, res) {
+  try {
+    const context = await req.json();
+    await sendEmail(context);
+    return new Response(JSON.stringify({ success: true, message: 'Email sent successfully' }), { status: 200 });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return new Response(JSON.stringify({ success: false, error: 'Email failed to send' }), { status: 500 });
+  }
+}
